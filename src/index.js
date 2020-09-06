2377e0975a48633d2ca978664f8ab6f876c9f616
midoghranek <midoghranek@hotmail.com>
2020-09-06 16:43:59 +0200

    👌 IMPROVE: add web components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import "./sass/main.scss";
+// Import styles
+import "./assets/sass/main.scss";
+import photo from "./assets/images/photo.jpg";
 
-const App = `<h1>Website Title</h1>`;
+// Web components
+class App extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <h1>HTML Boilerplate</h1>
+    <h2>Hello From Web Components</h2>
+    <img class="photo" src="${photo}"/>
+    `;
+  }
+}
 
+customElements.define("app-root", App);
+
+// DOM Edit
+const header = `<h4>Hello From JS DOM</h4>`;
 const appRoot = document.getElementById("app");
-appRoot.innerHTML = App;
+appRoot.innerHTML = header;
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   output: {
